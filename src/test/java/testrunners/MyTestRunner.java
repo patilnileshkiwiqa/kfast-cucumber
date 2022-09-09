df4cfa306c7213c957ad633dc176c11055ed75b2
patilnileshkiwiqa <nilesh.patil@kiwiqa.com>
2022-09-09 11:20:52 +0530

    First commit

---
+++ src/test/java/testrunners/MyTestRunner.java
@@ -0,0 +1,20 @@
+	package testrunners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/AppFeatures"},
+        glue = {"stepdefinations", "AppHooks"},
+        plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+       
+		)
+
+public class MyTestRunner {
+
+}
+
+ 
---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "a7ee4988-be78-4df5-9908-e2017cefe01e",
+    "feature": "Sauce Demo Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1659305524106,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1659305524167,
+    "className": "undefined"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
